2ca576ba94be21799a677b2180dab836e67a1817
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-05-03 14:45:26 +0300

    добавил Переделал полуфабрикаты страничек

---
+++ app/src/main/java/android/example/randomletter2/BrainTrainMenu.java
@@ -0,0 +1,14 @@
+package android.example.randomletter2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrainTrainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_train_menu);
+    }
+}
