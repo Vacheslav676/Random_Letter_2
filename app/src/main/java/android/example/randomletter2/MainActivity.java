0de043ba1642fe92b969e3f9212100a913a581a1
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-05-03 17:04:44 +0300

    добавил 2 страницы меню с переходом: SportButtonMenu и BrainTrainButtonMenu

--- app/src/main/java/android/example/randomletter2/MainActivity.java
+++ app/src/main/java/android/example/randomletter2/MainActivity.java
@@ -3,6 +3,7 @@ package android.example.randomletter2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -13,18 +14,46 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button SportButtonMenu;
+    Button BrainTrainButtonMenu;
 
        @Override
     protected void onCreate(Bundle savedInstanceState) {
            super.onCreate(savedInstanceState);
            setContentView(R.layout.activity_main);
 
+           SportButtonMenu = (Button) findViewById(R.id.sport_button_menu);
+           BrainTrainButtonMenu = (Button) findViewById(R.id.brain_train_button_menu);
+
+           SportButtonMenu.setOnClickListener(this);
+           BrainTrainButtonMenu.setOnClickListener(this);
+
        }
 
+    @Override
+    public void onClick(View v) {
 
+        Intent intent;
+        switch (v.getId()) {
+           case R.id.sport_button_menu:
+               // Создаю интент и говорю что хочу перейти из этого (this) активити,
+               // в активити2 = SportMenu.class
+                intent = new Intent(MainActivity.this, SportMenu.class);
+               //специальная функция для старта интента. После нее текущая активити
+               // не закрывается, а сворачивается
+                startActivity(intent);
+               //Сделал простейшие кнопки для перехода по активностям.
+               // Но каждая предыдущая активити будет висеть в буфере и в истории.
+                break;
+            case R.id.brain_train_button_menu:
+                intent = new Intent(MainActivity.this, BrainTrainMenu.class);
 
+                startActivity(intent);
+                break;
+        }
 
+    }
 
 }
