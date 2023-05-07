928e05a6adcf5984747e2208ea9fc82c25de4319
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-05-07 22:50:07 +0300

    перерисовываю таймер

--- app/src/main/java/android/example/randomletter2/MyTimer.java
+++ app/src/main/java/android/example/randomletter2/MyTimer.java
@@ -3,6 +3,7 @@ package android.example.randomletter2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MyTimer extends AppCompatActivity {
 
@@ -11,4 +12,15 @@ public class MyTimer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_timer);
     }
+
+    //Создаем слушателя кнопки, или обработчик события
+    View.OnClickListener onClickMyTimer = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+
+        }
+    }
+
+
 }
